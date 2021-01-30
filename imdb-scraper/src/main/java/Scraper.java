8f65e8398494e66ed0633a54ea8a022fb96a2a3d
SithWolf83 <ivan.ferrer1@gmail.com>
2021-01-30 16:51:04 +0100

    Added how to retrieve a table fields from a website example.

--- imdb-scraper/src/main/java/Scraper.java
+++ imdb-scraper/src/main/java/Scraper.java
@@ -1,14 +1,21 @@
-import java.io.IOException;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Scraper {
 
 	public static void main(String[] args) throws Exception {
 		
 		final Document document = Jsoup.connect("https://www.imdb.com/chart/top").get();
-		System.out.println(document.outerHtml());
+		
+		// "table" means the elements are in a table
+		// "chart.full-width" means the name of the table
+		// "tr" means is taking the rows of the table
+		for (Element row : document.select("table.chart.full-width tr")) {
+			final String title = row.select(".titleColumn a").text();		// "a" because we want <a href> tag.
+			final String rating = row.select(".imdbRating").text();
+			System.out.println(title + " -> Rating: " + rating);
+		}
 
 	}
 
